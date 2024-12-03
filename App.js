6753919aabd8508898655d0de0966e52d29fa979
Jolina John <52922687+jjohn342@users.noreply.github.com>
2024-12-03 10:55:29 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,46 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Blog from './Blog';
+import Forum from './Forum';
+import Account from './Account';
+import Contact from './Contact';
+import './app.css';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        {/* Global Header */}
+        <header>
+          <h1>CopperShore</h1>
+          <nav>
+            <a href="/blog">Blog</a>
+            <a href="/forum">Forum</a>
+            <a href="/account">Account</a>
+            <a href="/contact">Contact</a>
+          </nav>
+        </header>
+
+        {/* Routes for Pages */}
+        <Routes>
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/forum" element={<Forum />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Blog />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+export default App;
